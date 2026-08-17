129b8f3c30564bae2296be274fdcea21a6324491
Anurag Bahuguna <anurag.bahuguna777@gmail.com>
2026-08-17 23:17:23 +0530

    Update script2.js

--- Login_page/script2.js
+++ Login_page/script2.js
@@ -6,7 +6,6 @@ const loginBtn = document.getElementById('login');
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
-
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
